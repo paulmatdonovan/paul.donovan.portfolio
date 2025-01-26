5d99cdd8e1cb126563b2ddccf807cd8ab8b97215
Paul <paul.m.donovan@outlook.com>
2025-01-26 16:23:33 +1100

    changed css for Navbar responsive

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -4,13 +4,31 @@ import './Services.css';
 const Services = () => {
   return (
     <div className="services">
+      <div className="services-summary">
       <h1>Our Services</h1>
       <ul>
         <li>Custom Website Design</li>
+        <p>We will create your website from scratch, taking into account your needs and aspirations. We create effective no-nonsense websites that focus on one thing. Getting your brand or message to your target audience as clearly as possible.</p>
         <li>Responsive Web Development</li>
+        <p>Most people use their phones to access websites.
+We not only craft the best, most user friendly websites but make sure your website is accessible and easy to use on a mobile device.
+</p>
         <li>Content Writing</li>
+        <p>You can tell us your story in your own words or trust our capable team to craft the perfect message to sell your brand. </p>
         <li>Maintenance & Support</li>
+        <p>Some website jobs are easy. Others are more involved. A website will change as your product service or client change. There is nothing worse than a website without outdated content. Trust us to maintain and upgrade your site as required. </p>
       </ul>
+        </div>
+      <div className='mission'>
+      <h1 >My Mission</h1>
+      <p>My mission is to help regional businesses in the Wimmera to reach more customers, to attract more tourists and promote greater awareness of the exceptional products and services on offer in our region.
+Each website is built with love and showcases the unique qualities of your business or service.
+We use professional images and colours and the latest technologies to help you reach out to your customers. 
+</p>
+
+      </div>
+      
+
     </div>
   );
 };
